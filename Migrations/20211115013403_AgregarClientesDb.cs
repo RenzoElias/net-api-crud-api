147a7f1dcaa4193311f8a19f172db6bfd002d258
RenzoElias <renzoeliasdelacalle@gmail.com>
2021-11-15 19:20:51 -0500

    init app

---
+++ Migrations/20211115013403_AgregarClientesDb.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace APIClientes.Migrations
+{
+    public partial class AgregarClientesDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
